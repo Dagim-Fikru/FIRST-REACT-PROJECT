9ae6970e2fa0a1a010bca38db99c15904f59cdc6
Dagim-Fikru <95189710+Dagim-Fikru@users.noreply.github.com>
2022-03-16 10:23:27 +0300

    http request and useEffect

--- bbcasts/src/App.js
+++ bbcasts/src/App.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 import Header from './components/ui/header';
 
 
 const App = () => {
+  const [charcs, setCharcs] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+
+    const fetchItems = async() => {
+
+      const result = await axios(`https://www.breakingbadapi.com/api/characters`)
+      console.log(result.data)
+      setCharcs(result.data)
+      setIsLoading(false)
+    }
+    fetchItems()
+
+  }, [])
   return ( 
     <div className = "container" >
     
